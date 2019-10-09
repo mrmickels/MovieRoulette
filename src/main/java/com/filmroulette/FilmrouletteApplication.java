9507199f51133e39a678920b878ae75c6cb0067d
Gina Hopper <hopp.gina@gmail.com>
2019-10-08 23:06:31 -0400

    Update FilmrouletteApplication.java

--- src/main/java/com/filmroulette/FilmrouletteApplication.java
+++ src/main/java/com/filmroulette/FilmrouletteApplication.java
@@ -8,11 +8,8 @@ public class FilmrouletteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmrouletteApplication.class, args);
-		
-		//String parseTest = "";
 
         FetchData upcomingMovies = new FetchData();
-        //parseTest = upcomingMovies.fetchData();
         upcomingMovies.fetchData();
         upcomingMovies.parseData();
         
